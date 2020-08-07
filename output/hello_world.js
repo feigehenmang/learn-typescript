8b1032ed52a5fddd30a2793d3dbeb0256847f5d9
lijinfei <lijinfei@devcloud.com>
2020-08-07 09:47:00 +0800

    init

--- output/hello_world.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function show(msg) {
-    return msg.toUpperCase();
-}
-console.log(show("Hello World!"));
--- src/hello_world.ts
+++
@@ -1,4 +0,0 @@
-function show(msg: String): String {
-    return msg.toUpperCase();
-}
-console.log(show("Hello World!"));
